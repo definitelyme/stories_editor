68e9ba46fd9f98e634ab0dbba2d52bf6d858a42b
definitelyme <ejike.br@gmail.com>
2022-04-27 20:28:40 +0100

    bugfix

--- lib/src/presentation/utils/Extensions/list_extension.dart
+++ lib/src/presentation/utils/Extensions/list_extension.dart
@@ -5,3 +5,12 @@ extension NullIterableX<A> on Iterable<A> {
     return elementAt(index);
   }
 }
+
+extension NullListX<A> on List<A> {
+  A? elementAtOrNull(int? index) {
+    if (index == null || index < 0 || isEmpty || index >= length) return null;
+
+    return elementAt(index);
+  }
+}
+
